1b70d1d3c7c0008275eb0261b8b127468d68eb92
zahraa <zahraa.elsayed95@gmail.com>
2023-10-17 13:48:20 +0300

    m2

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { TaskContext } from "../context/TaskContext";
+import "./Task.css";
 
 const Task = ({ task }) => {
   const { setTaskList } = useContext(TaskContext);
@@ -12,10 +13,19 @@ const Task = ({ task }) => {
     // Implement delete functionality
   };
 
+  // Define styles for different status
+  const statusStyles = {
+    "Not Started": { color: "red" },
+    "In Progress": { color: "orange" },
+    Finished: { color: "green" },
+  };
+
   return (
-    <div>
-      <span>{task.description}</span>
-      <span>{task.status}</span>
+    <div className="task">
+      <span className="task-description">{task.description}</span>
+      <span className="task-status" style={statusStyles[task.status]}>
+        {task.status}
+      </span>
       <button onClick={handleEdit}>Edit</button>
       <button onClick={handleDelete}>Delete</button>
     </div>
